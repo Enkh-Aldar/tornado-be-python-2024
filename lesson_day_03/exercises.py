26b1b53cbd14be0b5d8de998a5e25c8d9bb58469
tornado-01 <tornado-01@ADMIN-01s-iMac.local>
2023-12-22 17:04:31 +0800

    add:lesson_day_03

--- lesson_day_03/exercises.py
+++ lesson_day_03/exercises.py
@@ -23,20 +23,36 @@
 # print(result)
 
 
-def convert_temperature():
-    try:
-        grad = input('Grad:')
-        temperature = int(input('Temperature:'))
+# def convert_temperature():
+#     try:
+#         grad = input('Grad:')
+#         temperature = int(input('Temperature:'))
     
-        if grad == "C":
-            result = temperature * (9/5) + 32
-        elif temperature == "C":
-            result = (temperature - 32) * 5/9
-    except:
-        print("Error")
+#         if grad == "C":
+#             result = temperature * (9/5) + 32
+#         elif temperature == "C":
+#             result = (temperature - 32) * 5/9
+#     except:
+#         print("Error")
             
-    print(result)
+#     print(result)
             
-convert_temperature()
+# convert_temperature()
 
-    
+
+def find_age(age):
+    try:
+        if (0 <= age) and (age <= 12):
+            print('You are a Child')
+        elif (age >= 13)and(age <= 19):
+            print('You are Teenager')
+        elif (age >= 20)and(age <= 59):
+            print('You are a Adult')
+        elif (age >= 60)and(age <= 100):
+            print('You are a Senior')
+        else:
+            print('you are dinosaur')
+    except:
+        print('You Are Dinosaur')
+        
+find_age(101)
