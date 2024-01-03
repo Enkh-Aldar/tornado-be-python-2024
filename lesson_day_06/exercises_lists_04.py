a12547b9fa31e4c48819d293b9bfb2195e7f9032
tornado-01 <tornado-01@ADMIN-01s-iMac.local>
2024-01-03 17:19:47 +0800

    add:lesson_day_06

--- lesson_day_06/exercises_lists_04.py
+++ lesson_day_06/exercises_lists_04.py
@@ -5,13 +5,6 @@ while True:
     num = input("Enter a number: ")
     if num == "done":
         break
-
-    try:
-        num = int(num)
-    except:
-        print("Invalid input")
-        continue
-
     if smallest == None or num < smallest:
         smallest = num
     if largest == None or num > largest:
