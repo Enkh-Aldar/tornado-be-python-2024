80789e74e29bceb8891da5e8ea3957f77a70b85d
Enkh-Aldar <aldaraa1245@gmail.com>
2024-01-29 20:16:31 +0800

    lesson_day_17

--- lesson_day_17/pages/urls.py
+++ lesson_day_17/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AboutPageView, HomePageView, InfoPageView
+from .views import AboutPageView, HomePageView, InfoPageView, LoginPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("info/", InfoPageView.as_view(), name="info"),
+    path("login/", LoginPageView.as_view(), name="login"),
 ]
