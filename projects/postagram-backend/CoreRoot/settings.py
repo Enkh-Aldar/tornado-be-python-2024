b997345bc5bdc6f310ecf537cae3ed2e393fc2ff
Enkh-Aldar <aldaraa1245@gmail.com>
2024-02-20 21:29:16 +0800

    postagram-backend

--- projects/postagram-backend/CoreRoot/settings.py
+++ projects/postagram-backend/CoreRoot/settings.py
@@ -39,8 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'core.user',
+    'core.auth',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS':
+    ['django_filters.rest_framework.DjangoFilterBackend'],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
