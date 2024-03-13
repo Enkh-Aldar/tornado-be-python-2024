da4aabcb2929c770f945b76514928ac5be029c31
tornado-01 <tornado-01@ADMIN-01s-iMac.local>
2024-03-13 15:30:50 +0800

    add:lesson day 33

---
+++ lesson_day_33/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-center'>React Front End Development</h1>
+      <img src="https://media.istockphoto.com/id/495388574/photo/the-worlds-largest-statue-of-genghis-khan.jpg?s=2048x2048&w=is&k=20&c=mHrJtsGggjCQko1tNEBOSy1Kby2thDb94bnGLIfI7HA=" alt="" className='w-1/2 rounded-md shadow-md mx-auto' />
+    </>
+  )
+}
+
+export default App;
